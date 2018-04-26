cb527ee5c4c0a651f807cad11c2c4dffdf117a70
Tobias Richling <trichling@gmx.de>
2018-04-26 11:57:21 +0200

    finished rmm level 1

--- RMM/Code/dotnetcologen.RichardsonMaturityModel.Api/Controllers/TimeBookingController.cs
+++ RMM/Code/dotnetcologen.RichardsonMaturityModel.Api/Controllers/TimeBookingController.cs
@@ -5,12 +5,12 @@ using dotnetCologne.RichardsonMaturityModel.Api.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.JsonPatch;
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers 
 {
 
-    [ApiExplorerSettings(IgnoreApi = true)]
-    [Route("timesheets/{name}/bookings")]
+    [Route("timesheets/bookings")]
     public class TimeBookingController : Controller
     {
 
@@ -21,6 +21,43 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers
             this.repostitory = repostitory;
         }
 
+        [HttpPost]
+        [Route("execute")]
+        [Consumes("application/json")]
+        public IActionResult Execute([FromQuery] string operation, [FromBody] JObject parameters)
+        {
+            switch (operation)
+            {
+                case "ShowAllTimebookings":
+                    var showAllTimebookingsRequest = parameters.ToObject<GetAllTimebookingsRequest>();
+                    var timesheet2 = repostitory.GetByName(showAllTimebookingsRequest.Timesheet);
+                    return Ok(timesheet2.Bookings);
+                
+                case "ShowTimebooking":
+                    var showTimebookingRequest = parameters.ToObject<GetTimebookingRequest>();
+                    var timesheet3 = repostitory.GetByName(showTimebookingRequest.Timesheet);
+                    return Ok(timesheet3.GetBookingByDate(showTimebookingRequest.Date));
+
+                case "CreateTimebooking":
+                    var createTimebookingRequest = parameters.ToObject<CreateTimebookingsRequest>();
+                    var timesheet4 = repostitory.GetByName(createTimebookingRequest.Timesheet);
+                    timesheet4.BookTime(createTimebookingRequest.Date, createTimebookingRequest.Start, createTimebookingRequest.Pause, createTimebookingRequest.End);
+                    repostitory.Save(timesheet4);
+                    return Ok();
+
+                case "UpdateTimebooking":
+                    var udpateTimebookingRequest = parameters.ToObject<UpdateimebookingRequest>();
+                    var timesheet5 = repostitory.GetByName(udpateTimebookingRequest.Timesheet);
+                    timesheet5.Update(udpateTimebookingRequest.Date, new TimeBooking(DateTime.MinValue, udpateTimebookingRequest.Start, udpateTimebookingRequest.Pause, udpateTimebookingRequest.End));
+                    repostitory.Save(timesheet5);
+                    return Ok();
+            }
+
+            return Ok();
+        }
+
+        #region "Ignored for RMM Level 1"
+        [ApiExplorerSettings(IgnoreApi = true)]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<TimeBooking>), 200)]
         public IActionResult GetAll([FromRoute] string name)
@@ -29,6 +66,7 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers
             return Ok(timesheet.Bookings);
         }
 
+        [ApiExplorerSettings(IgnoreApi = true)]
         [HttpGet]
         [Route("{date}", Name = "GetByDate")]
         [ProducesResponseType(typeof(TimeBooking), 200)]
@@ -38,6 +76,7 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers
             return Ok(timesheet.GetBookingByDate(date));
         }
 
+        [ApiExplorerSettings(IgnoreApi = true)]
         [HttpPost]
         [ProducesResponseType(typeof(TimeBooking), 201)]
         public IActionResult Create([FromRoute] string name, [FromBody] TimeBooking booking)
@@ -49,6 +88,7 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers
             return CreatedAtRoute("GetByDate", new { name = name, date = booking.Date }, timesheet.GetBookingByDate(booking.Date));
         }
 
+        [ApiExplorerSettings(IgnoreApi = true)]
         [HttpPut]
         [Route("{date}", Name = "GetByDate")]
         [ProducesResponseType(typeof(TimeBooking), 201)]
@@ -60,6 +100,7 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers
 
             return Ok(timesheet.GetBookingByDate(date));
         }
+        #endregion
     }
 
 }
--- RMM/Code/dotnetcologen.RichardsonMaturityModel.Api/Controllers/TimesheetsController.cs
+++ RMM/Code/dotnetcologen.RichardsonMaturityModel.Api/Controllers/TimesheetsController.cs
@@ -45,37 +45,13 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers {
                     repostitory.Delete(renameTimeSheetRequest.TimesheetToRename);
                     repostitory.Save(timesheet1);
                     return Ok();
-
-                case "ShowAllTimebookings":
-                    var showAllTimebookingsRequest = parameters.ToObject<GetAllTimebookingsRequest>();
-                    var timesheet2 = repostitory.GetByName(showAllTimebookingsRequest.Timesheet);
-                    return Ok(timesheet2.Bookings);
-                
-                case "ShowTimebooking":
-                    var showTimebookingRequest = parameters.ToObject<GetTimebookingRequest>();
-                    var timesheet3 = repostitory.GetByName(showTimebookingRequest.Timesheet);
-                    return Ok(timesheet3.GetBookingByDate(showTimebookingRequest.Date));
-
-                case "CreateTimebooking":
-                    var createTimebookingRequest = parameters.ToObject<CreateTimebookingsRequest>();
-                    var timesheet4 = repostitory.GetByName(createTimebookingRequest.Timesheet);
-                    timesheet4.BookTime(createTimebookingRequest.Date, createTimebookingRequest.Start, createTimebookingRequest.Pause, createTimebookingRequest.End);
-                    repostitory.Save(timesheet4);
-                    return Ok();
-
-                case "UpdateTimebooking":
-                    var udpateTimebookingRequest = parameters.ToObject<UpdateimebookingRequest>();
-                    var timesheet5 = repostitory.GetByName(udpateTimebookingRequest.Timesheet);
-                    timesheet5.Update(udpateTimebookingRequest.Date, new TimeBooking(DateTime.MinValue, udpateTimebookingRequest.Start, udpateTimebookingRequest.Pause, udpateTimebookingRequest.End));
-                    repostitory.Save(timesheet5);
-                    return Ok();
             }
 
             return Ok();
         }
 
 
-        #region "Ignored for RMM Level 0"
+        #region "Ignored for RMM Level 1"
         [ApiExplorerSettings(IgnoreApi = true)]
         [HttpGet]
         [ProducesResponseType(typeof(IEnumerable<Timesheet>), 200)]
