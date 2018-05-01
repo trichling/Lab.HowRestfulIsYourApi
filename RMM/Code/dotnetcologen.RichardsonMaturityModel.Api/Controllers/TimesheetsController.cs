a36887e0da50a5237afb2744615db48964cac8f1
Tobias Richling <trichling@gmx.de>
2018-05-01 10:43:15 +0200

    Reduce controller for demo

--- RMM/Code/dotnetcologen.RichardsonMaturityModel.Api/Controllers/TimesheetsController.cs
+++ RMM/Code/dotnetcologen.RichardsonMaturityModel.Api/Controllers/TimesheetsController.cs
@@ -32,35 +32,9 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers {
         {
             var result = repostitory.GetAll();
 
-            var sirenResultBuilder = new EntityBuilder()
-                .WithClass("timesheet")
-                .WithClass("collection")
-                .WithLink(new LinkBuilder()
-                    .WithRel("self")
-                    .WithHref(this.Url.Link("GetAll", new {}))
-                );
 
-            foreach (var timesheetEntry in result)
-            {
-                sirenResultBuilder
-                    .WithSubEntity(new EmbeddedLinkBuilder()
-                        .WithClass("timesheet")
-                        .WithTitle(timesheetEntry.Name)
-                        .WithRel("timesheet")
-                        .WithHref(this.Url.Link("GetByName", new { name = timesheetEntry.Name }))
-                    );
-            }
 
-            sirenResultBuilder.WithAction(new ActionBuilder()
-                .WithName("addTimesheet")
-                .WithTitle("Add a new Timesheet")
-                .WithMethod("POST")
-                .WithHref(this.Url.Link("GetAll", new {}))
-                .WithType("application/json")
-                .WithField(new FieldBuilder().WithName("Name").WithType("text"))
-            );
-
-            return Ok(sirenResultBuilder.Build());
+            return Ok(result);
         }
 
         [HttpGet]
@@ -75,45 +49,7 @@ namespace dotnetCologne.RichardsonMaturityModel.Api.Controllers {
 
             var timesheet = repostitory.GetByName(name);
 
-            var sirenResultBuilder = new EntityBuilder()
-                .WithClass("timesheet")
-                .WithProperty("Name", timesheet.Name)
-                .WithLink(new LinkBuilder()
-                    .WithRel("self")
-                    .WithHref(this.Url.Link("GetByName", new { name = timesheet.Name}))
-                );
-
-            foreach (var timebooking in timesheet.Bookings)
-            {
-                sirenResultBuilder
-                    .WithSubEntity(new EmbeddedRepresentationBuilder()
-                        .WithClass("timebooking")
-                        .WithProperty("Date", timebooking.Date)
-                        .WithProperty("Duration", timebooking.Duration)
-                        .WithRel("timebooking")
-                        .WithLink(new LinkBuilder() 
-                            .WithRel("timebooking")
-                            .WithHref(this.Url.Action("GetByDate", "TimeBooking", new { name = timesheet.Name, date = timebooking.Date }))
-                        )
-                    );
-            }
-
-            var href = this.Url.Action("Create", "TimeBooking", new { name = timesheet.Name});
-            var href1 = this.Url.Action("GetByDate", "TimeBooking", new { name = "Test", date = System.DateTime.Today });
-
-            sirenResultBuilder.WithAction(new ActionBuilder()
-                .WithName("addTimebooking")
-                .WithTitle("Add a new Timebooking")
-                .WithMethod("POST")
-                .WithHref(this.Url.Action("Create", "TimeBooking", new { name = timesheet.Name}))
-                .WithType("application/json")
-                .WithField(new FieldBuilder().WithName("Date").WithType("text"))
-                .WithField(new FieldBuilder().WithName("Start").WithType("text"))
-                .WithField(new FieldBuilder().WithName("Pause").WithType("text"))
-                .WithField(new FieldBuilder().WithName("End").WithType("text"))
-            );
-
-            return Ok(sirenResultBuilder.Build());
+            return Ok(timesheet);
         }
 
         [HttpPost]
